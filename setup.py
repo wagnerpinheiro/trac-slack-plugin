a198b2f5edee127ae69aa4f8879de80c2ffa3b2b
Ryan J Ollos <ryan.j.ollos@gmail.com>
2020-12-03 13:56:25 -0800

    Add dependencies to setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='SlackNotificationPlugin',
-    version='0.2',
+    version='0.3',
     description='Plugin to announce Trac changes in Slack',
     author='Wagner Pinheiro',
     url='https://github.com/wagnerpinheiro/trac-slack-plugin',
     license='BSD',
     #packages=['slack_notification'],
+    install_requires=['Trac', 'requests'],
     packages=find_packages(exclude=['*.tests']),
     classifiers=[
         'Framework :: Trac',
